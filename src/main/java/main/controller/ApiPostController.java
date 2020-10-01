ba2949bc30286d431ad9c3617d00ffa4e1df63fa
timur_guliev <timur.guliev@gmail.com>
2020-10-01 23:41:17 +0500

    01.10.2020

--- src/main/java/main/controller/ApiPostController.java
+++ src/main/java/main/controller/ApiPostController.java
@@ -1,10 +1,25 @@
 package main.controller;
 
+import main.api.response.PostResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiPostController {
+
+    private final PostResponse postResponse;
+
+    public ApiPostController(PostResponse postResponse) {
+        this.postResponse = postResponse;
+    }
+
+    @GetMapping("/api/post")
+    private PostResponse apiPost(@RequestParam int offset, @RequestParam int limit, @RequestParam String mode){
+
+        return postResponse;
+    }
+
 
 
 }
